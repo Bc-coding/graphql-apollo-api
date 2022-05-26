901224dc144d10f6f7ff8b6178dfeb93960b1b12
Belle Carrie <Bc.coding@hotmail.com>
2022-05-26 16:39:05 +0100

    user model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,26 +1,39 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+const bcryptSalt = process.env.BCRYPT_SALT;
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
     name: {
       type: String,
+      trim: trim,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
+      trim: true,
+      unique: true,
       required: true,
     },
     password: {
       type: String,
       required: true,
     },
-    // we need to define the relationship between user and task models
-    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
   },
-  // the timestamps will automatically have created_at and updated_at fields
   {
     timestamps: true,
   }
 );
+//we use the pre-save MongoDB hook to hash the password first in this part of the code.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const hash = await bcrypt.hash(this.password, Number(bcryptSalt));
+  this.password = hash;
+  next();
+});
 
 module.exports = mongoose.model("User", userSchema);
