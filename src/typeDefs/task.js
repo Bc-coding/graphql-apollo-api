ae4ea4c7b9a08d29acd59bbcfe354dc2d7c0fb00
Belle Carrie <Bc.coding@hotmail.com>
2022-05-26 20:52:55 +0100

    update task input

--- src/typeDefs/task.js
+++ src/typeDefs/task.js
@@ -2,12 +2,15 @@ const { gql } = require("apollo-server-express");
 
 module.exports = gql`
   extend type Query {
-    tasks: [Task!]
+    tasks(skip: Int, limit: Int): [Task!]
+    # tasks(cursor: String, limit: Int): [Task!]
     task(id: ID!): Task
   }
 
   extend type Mutation {
     createTask(input: createTaskInput!): Task
+    updateTask(id: ID!, input: updateTaskInput!): Task
+    deleteTask(id: ID!): Task
   }
   ###### TYPE ######
   type Task {
@@ -21,5 +24,10 @@ module.exports = gql`
     name: String!
     completed: Boolean!
     # userId: ID!   <--- we don't need this anymore. Once thee user logs in, we could get the userId from the database
+  }
+
+  input updateTaskInput {
+    name: String
+    completed: Boolean
   }
 `;
