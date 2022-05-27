3b3cabbcdb2e2ac575bda067834a8d054b353f63
Belle Carrie <Bc.coding@hotmail.com>
2022-05-27 18:24:54 +0100

    fixed sendEmail

--- src/utils/email/sendEmail.js
+++ src/utils/email/sendEmail.js
@@ -2,18 +2,35 @@ const nodemailer = require("nodemailer");
 const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
+const { log } = require("console");
 
 const sendEmail = async (email, subject, payload, template) => {
   try {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: 465,
+      port: 587,
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD, // naturally, replace both with your real credentials or an application-specific password
       },
     });
+
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    // let testAccount = await nodemailer.createTestAccount();
+
+    // // create reusable transporter object using the default SMTP transport
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: testAccount.user, // generated ethereal user
+    //     pass: testAccount.pass, // generated ethereal password
+    //   },
+    // });
 
     const source = fs.readFileSync(path.join(__dirname, template), "utf8");
     const compiledTemplate = handlebars.compile(source);
@@ -27,10 +44,14 @@ const sendEmail = async (email, subject, payload, template) => {
     };
 
     // Send email
-    transporter.sendMail(options(), (error, info) => {
+    await transporter.sendMail(options(), (error, info) => {
       if (error) {
         return error;
       } else {
+        console.log(info);
+
+        // Preview only available when sending through an Ethereal account
+        // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         return res.status(200).json({
           success: true,
         });
